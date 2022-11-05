4d11cedc89e0722aa7ca0de8c9e935747751ba52
austindeboer2 <90856458+austindeboer2@users.noreply.github.com>
2022-11-05 12:15:25 -0400

    Add files via upload

---
+++ FinalExam/Program.cs
@@ -0,0 +1,62 @@
+﻿public class Final
+{
+
+    public static void Main(string[] args)
+    {
+        //Feel free to use this space to write your own test to see how your code is working.
+        Final final = new Final();
+    }
+
+    //TODO: Finish the function to return a random integer between 1 and 10
+    //EX: example return values are 1,2,3,4,5,6,7,8,9, and 10
+    public int Random10()
+    {
+
+
+        return 0;//Delete line when you are ready to test this function.
+    }
+
+    //TODO: Given two integers, a and b, write code to return true when a is larger than b.
+    //If a and b are equal or b is larger than a you should return false. 
+    //EX: Given a = 10 and b = 3 the return value would be true, because a is larger than b.
+    public bool BigA(int a, int b)
+    {
+
+
+        return false;//Delete line when you are ready to test this function.
+    }
+
+    //TODO: Given three different integers, a, b, and c, write code to return the largest number. **The given numbers will all be different**
+    //EX: Given a = 1, b = 2, and c = 3, the return value would be 3 beacuse that is the largest value 
+    public int Biggest(int a, int b, int c)
+    {
+
+
+        return 0;//Delete line when you are ready to test this function.
+    }
+
+    //TODO: Given an integer (num) from 1-5, you must return the letter associated with that number given on the following table.
+    // 1 = A
+    // 2 = B
+    // 3 = C
+    // 4 = D
+    // 5 = E
+    //EX: Given num = 2, the return value should be B.
+    //NOTE:The letter you return must be capitalized. 
+    public string NumberToLetter(int num)
+    {
+
+
+        return "";//Delete line when you are ready to test this function.
+    }
+
+    //TODO: Given a string (word) and an integer (a) you must return a string that has the word repeated the number of times a specifies.
+    //EX: Given word = "fun" and a = 3, the return would be "funfunfun".
+    public string RepeatWord(string word, int a)
+    {
+
+
+        return "";//Delete line when you are ready to test this function.
+    }
+
+}
