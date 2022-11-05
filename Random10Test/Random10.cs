1f3b509fc080630c2cb709c3889d821a3225c0a5
austindeboer2 <90856458+austindeboer2@users.noreply.github.com>
2022-11-05 12:34:17 -0400

    fdas

--- Random10Test/Random10.cs
+++ Random10Test/Random10.cs
@@ -17,7 +17,7 @@ namespace Random10Test
             int found1 = 0;
             for (int i = 0; i < 10000; i++)
             {
-                if (final.Random10() == 1) found1++;
+                if (final.Random10() == 10) found1++;
             }
             if(found1 > 950 && found1 < 1050) Assert.Pass();
             else Assert.Fail("Your random distribution has falled outside of the acceptable limits. You may need to look at what random numbers you are generating."); 
